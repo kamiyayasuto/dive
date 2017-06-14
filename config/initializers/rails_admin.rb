7a0c440f5b006beb75f581ea504fd036e58fb9aa
kamiyayasuto <y.kamiya.201604@gmail.com>
2017-06-14 04:25:19 -0700

    re23

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,13 +3,13 @@ RailsAdmin.config do |config|
   ### Popular gems integration
 
   ## == Devise ==
-   config.authenticate_with do
-     warden.authenticate! scope: :user
-   end
-   config.current_user_method(&:current_user)
+  # config.authenticate_with do
+  #   warden.authenticate! scope: :user
+  # end
+  # config.current_user_method(&:current_user)
 
   ## == Cancan ==
-   config.authorize_with :cancan
+  # config.authorize_with :cancan
 
   ## == Pundit ==
   # config.authorize_with :pundit
@@ -21,7 +21,7 @@ RailsAdmin.config do |config|
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar true
+  # config.show_gravatar = true
 
   config.actions do
     dashboard                     # mandatory
